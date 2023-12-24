7e80670217aade2d89dc1a18d3c9a3ca450cd54b
Nia V <nia_vf@hotmail.com>
2023-12-24 16:21:02 +0000

    add code for rollbar functionality

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -32,6 +32,12 @@ import watchtower
 import logging
 from time import strftime
 
+# Rollbar -----------------------------
+# import os (already imported)
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 # HoneyComb ---------------------------
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
@@ -52,7 +58,7 @@ provider.add_span_processor(processor)
 # LOGGER.addHandler(cw_handler)
 # LOGGER.info("test log")
 
-# Show this is the logs within the backend-flask app (STDOUT)
+# Show this in the logs within the backend-flask app (STDOUT)
 simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
 provider.add_span_processor(simple_processor)
 
@@ -86,6 +92,29 @@ cors = CORS(
 #   timestamp = strftime('[%Y-%b-%d %H:%M]')
 #   LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
 #   return response
+
+# Rollbar -----------------------------
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+    
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
