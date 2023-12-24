e6b4ef1766d1624529863b3b8f183366270aee56
Nia V <nia_vf@hotmail.com>
2023-12-24 17:46:16 +0000

    rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -59,8 +59,8 @@ provider.add_span_processor(processor)
 # LOGGER.info("test log")
 
 # Show this in the logs within the backend-flask app (STDOUT)
-simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
-provider.add_span_processor(simple_processor)
+# simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+# provider.add_span_processor(simple_processor)
 
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
@@ -97,24 +97,24 @@ cors = CORS(
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
 @app.before_first_request
 def init_rollbar():
-    """init rollbar module"""
-    rollbar.init(
-        # access token
-        rollbar_access_token,
-        # environment name
-        'production',
-        # server root directory, makes tracebacks prettier
-        root=os.path.dirname(os.path.realpath(__file__)),
-        # flask already sets up logging
-        allow_logging_basic_config=False)
+  """init rollbar module"""
+  rollbar.init(
+    # access token
+    rollbar_access_token,
+    # environment name
+    'production',
+    # server root directory, makes tracebacks prettier
+    root=os.path.dirname(os.path.realpath(__file__)),
+    # flask already sets up logging
+    allow_logging_basic_config=False)
 
-    # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
-    
+  # send exceptions from `app` to rollbar, using flask's signal system.
+  got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+  
 @app.route('/rollbar/test')
 def rollbar_test():
-    rollbar.report_message('Hello World!', 'warning')
-    return "Hello World!"
+  rollbar.report_message('Hello World!', 'warning')
+  return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
